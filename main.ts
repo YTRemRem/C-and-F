f0dddf88b1a6314e48673929c75edfe1840e3831
YTRemRem <43708359+YTRemRem@users.noreply.github.com>
2021-08-05 09:55:14 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    C = 5
+    F = C * 9 / 5 + 32
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(F)
+})
+let F = 0
+let C = 0
+let x = 0
+basic.forever(function () {
+	
+})
